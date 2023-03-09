eaf3cca82f7334a42cf1053d4b0330035e400769
Avilova21 <avilova21dk@gmail.com>
2023-03-09 11:30:06 +0500

    add dynamic import and create transition group

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,21 +1,31 @@
-import { Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import { MainPage, ComicsPage, Page404, SingleComicPage } from "../pages";
 import AppHeader from "../appHeader/AppHeader";
+import Spinner from "../spinner/Spinner";
+
+const Page404 = lazy(() => import("../pages/404"));
+const MainPage = lazy(() => import("../pages/MainPage"));
+const ComicsPage = lazy(() => import("../pages/ComicsPage"));
+const SingleComicPage = lazy(() => import("../pages/SingleComicPage"));
 
 const App = () => {
 	return (
-		<div className="app">
-			<AppHeader/>
-			<main>
-				<Routes>
-					<Route path="/" element={<MainPage/>}/>
-					<Route path="/comics" element={<ComicsPage/>}/>
-					<Route path="/comics/:comicId" element={<SingleComicPage/>}/>
-					<Route path="*" element={<Page404/>}/>
-				</Routes>
-			</main>
-		</div>
+		<BrowserRouter>
+			<div className="app">
+				<AppHeader/>
+				<main>
+					<Suspense fallback={<Spinner/>}>
+						<Routes>
+							<Route path="/" element={<MainPage/>}/>
+							<Route path="/comics" element={<ComicsPage/>}/>
+							<Route path="/comics/:comicId" element={<SingleComicPage/>}/>
+							<Route path="*" element={<Page404/>}/>
+						</Routes>
+					</Suspense>
+				</main>
+			</div>
+		</BrowserRouter>
 	)
 }
 
--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/app/App";
 
-import { BrowserRouter } from "react-router-dom";
-
 import "./style/style.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<BrowserRouter>
 		<React.StrictMode>
 			<App/>
 		</React.StrictMode>
-	</BrowserRouter>
 );
 

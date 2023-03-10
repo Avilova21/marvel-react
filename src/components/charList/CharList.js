eae088fa98a3f9ed57fa1fd05e4eed385a177179
Avilova21 <avilova21dk@gmail.com>
2023-03-11 00:25:48 +0500

    create state machine create meta tag

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import {useState, useEffect, useRef, useMemo} from "react";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import PropTypes from "prop-types";
@@ -7,6 +7,21 @@ import { CSSTransition, TransitionGroup } from "react-transition-group"
 import useMarvelService from "../../services/MarvelService";
 import "./charList.scss";
 
+const setContent = (process, Component, newItemLoading) => {
+	switch (process) {
+		case "waiting":
+			return <Spinner/>;
+		case "loading":
+			return newItemLoading ? <Component/> : <Spinner/>;
+		case "confirmed":
+			return <Component/>;
+		case "error":
+			return <ErrorMessage/>;
+		default:
+			throw new Error("Unexpected process state");
+	}
+}
+
 const CharList = (props) => {
 
 	const [charList, setCharList] = useState([]);
@@ -14,7 +29,7 @@ const CharList = (props) => {
 	const [offset, setOffset] = useState(210);
 	const [charEnded, setCharEnded] = useState(false);
 
-	const { loading, error, getAllCharacters } = useMarvelService();
+	const {getAllCharacters, setProcess, process} = useMarvelService();
 
 	useEffect(() => {
 		onRequest(offset, true);
@@ -24,6 +39,7 @@ const CharList = (props) => {
 		initial ? setNewItemLoading(false) : setNewItemLoading(true);
 		getAllCharacters(offset)
 			.then(onCharListLoaded)
+			.then(() => setProcess('confirmed'))
 	}
 
 	const onCharListLoaded = (newCharList) => {
@@ -85,16 +101,13 @@ const CharList = (props) => {
 		)
 	}
 
-	const items = renderItems(charList);
-
-	const errorMessage = error ? <ErrorMessage/> : null;
-	const spinner = loading && !newItemLoading ? <Spinner/> : null;
+	const elements = useMemo(() => {
+		return setContent(process, () => renderItems(charList), newItemLoading);
+	}, [process]);
 
 	return (
 		<div className="char__list">
-			{errorMessage}
-			{spinner}
-			{items}
+			{elements}
 			<button
 				className="button button__main button__long"
 				disabled={newItemLoading}
--- src/components/pages/SinglePage.js
+++ src/components/pages/SinglePage.js
@@ -1,16 +1,14 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
-import ErrorMessage from "../errorMessage/ErrorMessage";
 import useMarvelService from "../../services/MarvelService";
-import Spinner from "../spinner/Spinner";
 import AppBanner from "../appBanner/AppBanner";
-
+import setContent from "../../utils/setContent";
 
 const SinglePage = ({ Component, dataType }) => {
 	const {id} = useParams();
 	const [data, setData] = useState(null);
-	const {loading, error, getComic, getCharacter, clearError} = useMarvelService();
+	const {getComic, getCharacter, clearError, process, setProcess} = useMarvelService();
 
 	useEffect(() => {
 		updateData()
@@ -21,10 +19,13 @@ const SinglePage = ({ Component, dataType }) => {
 
 		switch (dataType) {
 			case "comic":
-				getComic(id).then(onDataLoaded);
+				getComic(id).then(onDataLoaded).then(() => setProcess("confirmed"))
 				break;
 			case "character":
-				getCharacter(id).then(onDataLoaded);
+				getCharacter(id).then(onDataLoaded).then(() => setProcess("confirmed"))
+				break;
+			default:
+				return;
 		}
 	}
 
@@ -32,19 +33,12 @@ const SinglePage = ({ Component, dataType }) => {
 		setData(data);
 	}
 
-	const errorMessage = error ? <ErrorMessage/> : null;
-	const spinner = loading ? <Spinner/> : null;
-	const content = !(loading || error || !data) ? <Component data={data}/> : null;
-
 	return (
 		<>
 			<AppBanner/>
-			{errorMessage}
-			{spinner}
-			{content}
+			{setContent(process, Component, data)}
 		</>
 	)
 }
-
 
 export default SinglePage;
